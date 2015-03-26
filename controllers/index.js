6a2558262141fd336c15262a60ac1d47bf9020a4
Matt Rich <metropolis_4@hotmail.com>
2015-03-25 23:13:58 -0600

    changes for heroku deployment api keys

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
-var linkedin = require('../config/linkedInAPI.js');
+// var linkedin = require('../config/linkedInAPI.js');
 var indexController = {
 	index: function(req, res) {
 		res.render('index');
 	},
     linkedIn: function(req, res) {
-        res.redirect('https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=' + linkedin.key + '&redirect_uri=https://shielded-everglades-7672.herokuapp.com/main&state=5150515051505150');
+        res.redirect('https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=' + process.env.key + '&redirect_uri=https://shielded-everglades-7672.herokuapp.com/main&state=5150515051505150');
     },
     getMain: function(req, res){
         res.render('main');
