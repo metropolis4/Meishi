38c0ec729d42b6dcc90c40cea9d4dacc6e45629d
Matt Rich <metropolis_4@hotmail.com>
2015-03-25 21:11:27 -0600

    Updated Dependencies

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var indexController = require('./controllers/index.js');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    indexController = require('./controllers/index.js');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/meishi');
 
 app.get('/', indexController.index);
 
-var server = app.listen(6403, function() {
+var port = process.env.PORT || 6403;
+var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
