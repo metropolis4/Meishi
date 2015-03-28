92ad9415b8c7d2137fa373d2a949e081e6601c68
Matt Rich <metropolis_4@hotmail.com>
2015-03-27 22:52:46 -0600

    fixing API routes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -14,7 +14,7 @@ meishi.factory('User', function($resource){
 meishi.controller('mainController', ['$scope', 'User', function($scope, User){
     
     var user = User.items;
-    user = user[0].linkedin;
+    user = user.linkedin;
     console.log("FROM ANGULAR::: ", user);
     $scope.userInfo = User.items;
 }]);
