f6d1a56970c55e0d1efa098f367d82ec1c299305
Matt Rich <metropolis_4@hotmail.com>
2015-03-27 22:47:52 -0600

    fix res.redirect order

--- controllers/index.js
+++ controllers/index.js
@@ -29,7 +29,7 @@ var indexController = {
             });
             user.save();
 
-            return res.redirect('/main', { user: user});
+            return res.redirect({ user: user}, '/main');
         });
     },
     getProfile: function(req, res){
