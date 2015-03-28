9c8e859e1eba0f978dedcd7674930fe5586d8630
Matt Rich <metropolis_4@hotmail.com>
2015-03-27 22:58:13 -0600

    fixing data structure ngresource

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -13,9 +13,10 @@ meishi.factory('User', function($resource){
 });
 meishi.controller('mainController', ['$scope', 'User', function($scope, User){
     
-    var user = User.items;
-    user = _.map(user, function(val){
-        return val;
+    var user = User.model.query(function(user){
+        _.map(user, function(val){
+            return val;
+        });
     });
     console.log("FROM ANGULAR::: ", user);
     $scope.userInfo = user;
