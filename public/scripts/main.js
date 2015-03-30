b50405069a98b15c084b4a6570443866884e35fe
Matt Rich <metropolis_4@hotmail.com>
2015-03-30 16:35:54 -0600

    Testing routes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,7 @@
 (function(){
 var meishi = angular.module('meishi', ['ngResource']);
 meishi.factory('User', function($resource){
-    var model = $resource('/getProfile/:access_token', {access_token: '@access_token'}, {
+    var model = $resource('/getProfile/:id', {id: '@id'}, {
         update: {
             method: 'PUT',
         }
@@ -13,7 +13,7 @@ meishi.factory('User', function($resource){
 });
 meishi.controller('mainController', ['$scope', '$http' , 'User', function($scope, $http, User){
     
-    var user = User.model.get({});
+    var user = User.items;
     user = user;
     $scope.greeting = "Hello";
     $http.get('https://api.linkedin.com/v1/people/~?format=json')
@@ -23,7 +23,7 @@ meishi.controller('mainController', ['$scope', '$http' , 'User', function($scope
             .error(function(data, status, headers, config){
                 $scope.user2 = status;
             });
-    console.log("FROM ANGULAR::: ", user, "TRY AGAIN??? ", User.items);
+    console.log("FROM ANGULAR::: ", user, "TRY AGAIN??? ", User.model.get({}));
     $scope.userInfo = user;
 }]);
 })();
